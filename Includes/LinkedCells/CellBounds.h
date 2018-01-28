e8c6a63ef5a0ed227e19ec6b8754576cc2459b70
Ludewig François <francoisludewig@gmail.com>
2018-01-28 20:04:26 +0100

    Update CellBounds

--- Includes/LinkedCells/CellBounds.h
+++ Includes/LinkedCells/CellBounds.h
@@ -4,12 +4,22 @@ class CellBounds {
 public:
 	CellBounds() noexcept :
 		minX(0), minY(0), minZ(0),
-		maxX(0), maxY(0), maxZ(0) {}
+		maxX(0), maxY(0), maxZ(0),
+		startX(0), startY(0), startZ(0),
+		endX(0), endY(0), endZ(0),
+		lx(0), ly(0), lz(0){}
 
 	CellBounds(int minX, int minY, int minZ,
-			   int maxX, int maxY, int maxZ) noexcept :
+			   int maxX, int maxY, int maxZ,
+			   int startX, int startY, int startZ,
+			   int endX, int endY, int endZ,
+			   double lx, double ly, double lz) noexcept :
 			minX(minX), minY(minY), minZ(minZ),
-			maxX(maxX), maxY(maxY), maxZ(maxZ) {}
+			maxX(maxX), maxY(maxY), maxZ(maxZ),
+			startX(startX), startY(startY), startZ(startZ),
+			endX(endX), endY(endY), endZ(endZ),
+			lx(lx), ly(ly), lz(lz){}
+
 
 	CellBounds(const CellBounds& other) noexcept = default;
 	CellBounds(CellBounds&& other) noexcept = default;
@@ -24,8 +34,20 @@ public:
 	int MaxX() const noexcept { return maxX;}
 	int MaxY() const noexcept { return maxY;}
 	int MaxZ() const noexcept { return maxZ;}
+	int StartX() const noexcept { return startX;}
+	int StartY() const noexcept { return startY;}
+	int StartZ() const noexcept { return startZ;}
+	int EndX() const noexcept { return endX;}
+	int EndY() const noexcept { return endY;}
+	int EndZ() const noexcept { return endZ;}
+	double Lx() const noexcept { return lx;}
+	double Ly() const noexcept { return ly;}
+	double Lz() const noexcept { return lz;}
 
 private:
 	int minX, minY, minZ;
 	int maxX, maxY, maxZ;
+	int startX, startY, startZ;
+	int endX, endY, endZ;
+	double lx, ly, lz;
 };
