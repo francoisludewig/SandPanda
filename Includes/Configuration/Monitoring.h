c3ba6927fd4b31b1aff00d7ac815818167ef9838
fanstein <francois.ludewig@gmail.com>
2025-03-23 15:48:33 +0100

    Add monitoring classes and script

---
+++ Includes/Configuration/Monitoring.h
@@ -0,0 +1,20 @@
+//
+// Created by ludfr on 25/02/25.
+//
+
+#pragma once
+
+#include <string>
+#include <utility>
+#include <fstream>
+#include <chrono>
+
+class Monitoring {
+public:
+    explicit Monitoring(std::string id, std::string filename);
+    void metrics(const double current, const double final) const;
+
+private:
+    std::string id;
+    std::string filename;
+};
---
+++ bridgeToInflux.py
@@ -0,0 +1,42 @@
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+import os
+import sys
+
+token = "ODOAfu91GCSOA7vpeNLMKsZGtZQQsMM2mI9YHIhxm_WmX4xLFTJzOADtY3C-nbysbaDtQVdOohpYkQjeMPKFcA=="
+org = "FiFr"
+url = "http://127.0.0.1:8086"
+
+# Fichier source
+FICHIER_SOURCE = "/home/ludfr/MonSandPanda.txt"
+
+def lire_et_encoder(nom, niveau_actuel, niveau_max):
+    """Lit le fichier, enregistre les données dans InfluxDB et efface le contenu du fichier."""
+    try:
+        # Connexion à InfluxDB
+        client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)        
+        bucket="fanstein"
+        write_api = client.write_api(write_options=SYNCHRONOUS)
+        point = (
+        Point("sandPanda")
+        .tag("id", nom)
+        .tag("host", "atoem")
+        .field("now", (float)(niveau_actuel))
+        .field("final", (float)(niveau_max))
+        )
+        write_api.write(bucket=bucket, org="FiFr", record=point)
+        print(f"Donnée insérée: {nom, niveau_actuel, niveau_max}")
+
+        # Fermer la connexion
+
+        client.close()
+
+        print("Données insérées")
+
+    except Exception as e:
+        print(f"Erreur: {e}")
+
+if __name__ == "__main__":
+    lire_et_encoder(sys.argv[1], sys.argv[2], sys.argv[3])
