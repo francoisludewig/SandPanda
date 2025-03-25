20dd9439209473858978141ef43d59435b97b66c
fanstein <francois.ludewig@gmail.com>
2025-03-25 07:39:06 +0100

    Revert "monitoring of ram and relative with python script"
    This reverts commit 0b21a526e326ca59ba642b4914efe50e0b971542.

--- Sources/Configuration/Monitoring.cpp
+++ Sources/Configuration/Monitoring.cpp
@@ -15,6 +15,6 @@ void Monitoring::metrics(const double current, const double final) const {
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
     char command[1024];
-    sprintf(command, "/home/ludfr/.venv/bin/python3 /home/ludfr/IntegrationTests/dev_refactore/SandPanda/bridgeToInflux.py %s %f %f %d", Monitoring::id.c_str(), current, final, pid);
+    sprintf(command, "/home/ludfr/.venv/bin/python3 /home/ludfr/monitoring/bridgeToInflux.py %s %f %f %d", Monitoring::id.c_str(), current, final, pid);
     system(command);
 }
